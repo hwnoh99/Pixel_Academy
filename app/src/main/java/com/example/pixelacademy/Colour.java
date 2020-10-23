c65fe37408e4c1fb09027e6fd1160808e1d741ca
Noh Hyunwoo <coelacenth@gmail.com>
2020-10-23 23:34:54 +0800

    Run on real device if you want to use camera Version for AAD assignment submission Layout is optimized for Pixel 3a

--- app/src/main/java/com/example/pixelacademy/Colour.java
+++ app/src/main/java/com/example/pixelacademy/Colour.java
@@ -9,6 +9,8 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.util.Log;
 
+import android.provider.MediaStore;
+
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
@@ -27,9 +29,24 @@ public class Colour extends AppCompatActivity {
         setContentView(R.layout.activity_colour);
 
         final Button button = (Button)findViewById(R.id.button1);
+        final Button button2 = (Button)findViewById(R.id.button2);
         final ImageView iv = (ImageView)findViewById(R.id.imageView);
-        final TextView tv = (TextView)findViewById(R.id.textView1);
 
+        final TextView tv = (TextView)findViewById(R.id.textView1);
+        Bitmap bitmap;
+
+        //camera button
+        button.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE); //Run camera app when user click
+                startActivity(intent);
+            }
+        });
+
+        //google photo button
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -40,51 +57,79 @@ public class Colour extends AppCompatActivity {
             }
         });
 
-//        iv.setOnTouchListener(new View.OnTouchListener() {
-//            @Override
-//            public boolean onTouch(View v, MotionEvent event) {
-//                if (event.getAction() == MotionEvent.ACTION_DOWN){
-//                    tv.setText("Touch coordinates : " +
-//                            String.valueOf(event.getX()) + "x" + String.valueOf(event.getY()));
-//                }
-//                return true;
-//            }
-//        });
+        iv.setOnTouchListener(new OnTouchListener(){
+            @Override
+            public boolean onTouch(View v, MotionEvent event){
+                //Get the Pixel coordinates where touched
+                int x = (int)event.getX();
+                int y = (int)event.getY();
+                String x_str = Integer.toString(x);
+                String y_str = Integer.toString(y);
 
+                iv.buildDrawingCache();
+                Bitmap bitmap = iv.getDrawingCache(); // Create bitmap from image selected
 
+                int pixel = bitmap.getPixel(x,y);
 
-//        iv.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                tv.setText("Hello World!");
-//            }
-//        });
+                //get pixel variables
+                int redValue = Color.red(pixel);
+                int blueValue = Color.blue(pixel);
+                int greenValue = Color.green(pixel);
+                int alphaValue = Color.alpha(pixel);
+
+                printLocation(x_str, y_str, redValue, greenValue, blueValue, alphaValue);
+
+                return true;
+            }
+        });
     }
 
-    @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-
-        final ImageView iv = (ImageView)findViewById(R.id.imageView);
-
         // Check the request we're responding to
         if (requestCode == 1) {
             if (resultCode == RESULT_OK) {
                 try {
+                    final ImageView iv = (ImageView)findViewById(R.id.imageView);
                     // Create bitmap from image selected
                     InputStream in = getContentResolver().openInputStream(data.getData());
                     Bitmap img = BitmapFactory.decodeStream(in);
                     in.close();
-                    // display image
+
+                    //Output the image file's name
                     iv.setImageBitmap(img);
-                } catch (Exception e) {
+                    String result = data.getDataString();
+                    printFileName(result);
+                }
+                catch (Exception e) {
                     Log.d("Tag", "Error");
                 }
             }
         }
-
     }
 
-    int RGB = android.graphics.Color.argb(255, Red, Green, Blue);
+    public void printFileName (String x) {
+        final TextView tv = (TextView)findViewById(R.id.textView1);
+        tv.setText(x);
+    }
+    public void printLocation (String x, String y, int r, int g, int b, int a) {
+        //Set text with the parameter gotten
+        final TextView tvR = (TextView)findViewById(R.id.textView_R);
+        final TextView tvG = (TextView)findViewById(R.id.textView_G);
+        final TextView tvB = (TextView)findViewById(R.id.textView_B);
+        final TextView tvA = (TextView)findViewById(R.id.textView4);
+
+        final TextView tvX = (TextView)findViewById(R.id.textView_X);
+        final TextView tvY = (TextView)findViewById(R.id.textView_Y);
+
+        tvR.setText("R: " + r);
+        tvG.setText("G: " + g);
+        tvB.setText("B: " + b);
+        tvA.setText("A: " + a);
+
+        tvX.setText("X: " + x);
+        tvY.setText("Y: " + y);
+
+    }
 
 }
 
--- app/src/main/java/com/example/pixelacademy/MainActivity.java
+++ app/src/main/java/com/example/pixelacademy/MainActivity.java
@@ -6,9 +6,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.content.Intent;
 import android.widget.Button;
-import android.widget.ImageView;
-import java.io.File;
+import android.widget.Toast;
 
+import android.widget.ImageView;
+import android.net.Uri;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,11 +20,25 @@ public class MainActivity extends AppCompatActivity {
 
 
         final Button button = (Button)findViewById(R.id.button1);
+        final Button button2 = (Button)findViewById(R.id.button2);
 
+        //button for upload picture activity
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), Colour.class);
+                startActivity(intent);
+            }
+        });
+
+        //Link button to access the app's Github page
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(v.getContext(), "Accessing GitHub by Pixel Academy", Toast.LENGTH_LONG).show();
+                // Pop up toast message for long period
+
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/hwnoh99/Pixel_Academy"));
                 startActivity(intent);
             }
         });
